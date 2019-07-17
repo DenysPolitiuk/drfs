f9fce04153d6cfd7fcc0c86f42f96de502435d4a
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-07-16 21:55:21 -0400

    Added clap and accept target to process

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use clap::{App, Arg};
+
 use drfs::Entry;
+
+use std::env;
 
 // TODO:
 //
@@ -19,8 +23,23 @@ use drfs::Entry;
 //      * Optionally load from permanent storage
 //  * Optionally use TUI to go through results
 fn main() {
-    let target_name = "/home/boris/";
-    let mut entry = match Entry::new(target_name) {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .takes_value(true)
+                .help("target to process"),
+        )
+        .get_matches();
+
+    let target_name = matches
+        .value_of("target")
+        .or(env::current_dir().unwrap().to_str())
+        .unwrap()
+        .to_owned();
+    let mut entry = match Entry::new(&target_name) {
         Err(e) => panic!("{}", e),
         Ok(v) => v,
     };
